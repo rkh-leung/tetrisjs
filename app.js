8fc709a68cadf0a460240ab8b61a0778648594a6
rkh-leung <leptanian@gmail.com>
2020-05-20 08:30:42 +1200

    Add lTetromino and rotation variant

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  return null;
+  const GRID = document.querySelector('grid');
+  const SCORE_DISPLAY = document.querySelector('#score')
+  const START_BTN = document.querySelector('#start-button')
+  const WIDTH = 10;
+  let squares = Array.from(document.querySelectorAll('grid div'))
+
+  // The Tetrominoes
+  const lTetromino = [
+      [1, WIDTH+1, WIDTH*2+1, 2],
+      [WIDTH, WIDTH+1, WIDTH+2, WIDTH*2+2],
+      [1, WIDTH+1, WIDTH*2+1, WIDTH*2],
+      [WIDTH, WIDTH*2, WIDTH*2+1, WIDTH*2+2]
+  ]
 });
